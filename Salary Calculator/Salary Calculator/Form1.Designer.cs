a09c9968d8cce334428e48060b85bf07fb92bf64
shijumylapra <shijumylapra@gmail.com>
2024-06-25 03:52:49 -0700

    initial commit

---
+++ Salary Calculator/Salary Calculator/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Salary_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label5 = new System.Windows.Forms.Label();
+            this.lv_Calc = new System.Windows.Forms.ListView();
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txt_inp3 = new System.Windows.Forms.TextBox();
+            this.txt_Error = new System.Windows.Forms.TextBox();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_in1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Pay Amount";
+            this.columnHeader9.Width = 120;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Straight Pay";
+            this.columnHeader7.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Hourly Rate";
+            this.columnHeader6.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "OT Hours";
+            this.columnHeader2.Width = 75;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Regular Hrs";
+            this.columnHeader1.Width = 85;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(-3, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Created by SHIJU ABRHAM";
+            // 
+            // lv_Calc
+            // 
+            this.lv_Calc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lv_Calc.BackColor = System.Drawing.Color.LightGray;
+            this.lv_Calc.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.lv_Calc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lv_Calc.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lv_Calc.ForeColor = System.Drawing.Color.Maroon;
+            this.lv_Calc.FullRowSelect = true;
+            this.lv_Calc.GridLines = true;
+            this.lv_Calc.HideSelection = false;
+            this.lv_Calc.Location = new System.Drawing.Point(3, 5);
+            this.lv_Calc.Name = "lv_Calc";
+            this.lv_Calc.Size = new System.Drawing.Size(551, 219);
+            this.lv_Calc.TabIndex = 2;
+            this.lv_Calc.UseCompatibleStateImageBehavior = false;
+            this.lv_Calc.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "OT Pay";
+            this.columnHeader8.Width = 75;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.lv_Calc);
+            this.panel3.Location = new System.Drawing.Point(12, 212);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(559, 227);
+            this.panel3.TabIndex = 3;
+            // 
+            // txt_inp3
+            // 
+            this.txt_inp3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_inp3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_inp3.Location = new System.Drawing.Point(137, 55);
+            this.txt_inp3.Name = "txt_inp3";
+            this.txt_inp3.Size = new System.Drawing.Size(298, 29);
+            this.txt_inp3.TabIndex = 9;
+            // 
+            // txt_Error
+            // 
+            this.txt_Error.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txt_Error.Enabled = false;
+            this.txt_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Error.ForeColor = System.Drawing.Color.Black;
+            this.txt_Error.HideSelection = false;
+            this.txt_Error.Location = new System.Drawing.Point(137, 153);
+            this.txt_Error.Name = "txt_Error";
+            this.txt_Error.Size = new System.Drawing.Size(298, 29);
+            this.txt_Error.TabIndex = 8;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(294, 99);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(141, 38);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(137, 99);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(138, 38);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hourly Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Hours Worked";
+            // 
+            // txt_in1
+            // 
+            this.txt_in1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_in1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_in1.Location = new System.Drawing.Point(137, 17);
+            this.txt_in1.Name = "txt_in1";
+            this.txt_in1.Size = new System.Drawing.Size(298, 31);
+            this.txt_in1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.txt_inp3);
+            this.panel2.Controls.Add(this.txt_Error);
+            this.panel2.Controls.Add(this.btn_exit);
+            this.panel2.Controls.Add(this.btn_clear);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txt_in1);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(556, 193);
+            this.panel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 444);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView lv_Calc;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txt_inp3;
+        private System.Windows.Forms.TextBox txt_Error;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_in1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
